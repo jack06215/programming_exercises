664dfb624bb7dd69505875fd541ca6081cbab462
nacho0654 <jack06215@gmail.com>
2021-02-11 16:33:18 +0800

    sorting

--- LeetCode Exercises/C++STL Examples/sorting.cpp
+++ LeetCode Exercises/C++STL Examples/sorting.cpp
@@ -6,15 +6,42 @@
 #include <vector>
 #include "../prettyprint.h"
 
-template <typename T> void insertion_sort(T start, T end) {
-    for (auto i = start; i != end; ++i) {
-        std::rotate(std::upper_bound(start, i, *i), i, std::next(i));
+template<class FwdIt, class Compare = std::less<>>
+void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
+{
+    for (auto it = first; it != last; ++it) {
+        auto const insertion = std::upper_bound(first, it, *it, cmp);
+        std::rotate(insertion, it, std::next(it)); 
     }
+}
+
+template<class FwdIt, class Compare = std::less<>>
+void selection_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
+{
+    for (auto it = first; it != last; ++it) {
+        auto const selection = std::min_element(it, last, cmp);
+        std::iter_swap(selection, it); 
+    }
+}
+
+template<class FwdIt, class Compare = std::less<>>
+void quick_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
+{
+	auto const N = std::distance(first, last);
+	if (N <= 1){ 
+        return;
+    }
+	auto const pivot = std::next(first, N / 2);
+	std::nth_element(first, pivot, last, cmp);
+	quick_sort(first, pivot, cmp);
+	quick_sort(pivot, last, cmp);
 }
 
 int main() {
     std::vector<int> arr{4, 8, 6, -1, -2, -3, -1, 3, 4, 5};
-    insertion_sort(arr.begin(), arr.begin());
+    // insertion_sort(begin(arr), end(arr));
+    // quick_sort(begin(arr), end(arr));
+    selection_sort(begin(arr), end(arr));
 
     std::cout << arr << std::endl;
     return 0;
