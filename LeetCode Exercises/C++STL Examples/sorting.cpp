8d41a983f1a46965ccc1ae3be7c9b872c5edbd34
nacho0654 <jack06215@gmail.com>
2021-02-11 21:06:57 +0800

    test

--- LeetCode Exercises/C++STL Examples/sorting.cpp
+++ LeetCode Exercises/C++STL Examples/sorting.cpp
@@ -7,8 +7,7 @@
 #include "../prettyprint.h"
 
 template<class FwdIt, class Compare = std::less<>>
-void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
-{
+void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{}) {
     for (auto it = first; it != last; ++it) {
         auto const insertion = std::upper_bound(first, it, *it, cmp);
         std::rotate(insertion, it, std::next(it)); 
@@ -16,8 +15,7 @@ void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 }
 
 template<class FwdIt, class Compare = std::less<>>
-void selection_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
-{
+void selection_sort(FwdIt first, FwdIt last, Compare cmp = Compare{}) {
     for (auto it = first; it != last; ++it) {
         auto const selection = std::min_element(it, last, cmp);
         std::iter_swap(selection, it); 
@@ -25,12 +23,9 @@ void selection_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 }
 
 template<class FwdIt, class Compare = std::less<>>
-void quick_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
-{
+void quick_sort(FwdIt first, FwdIt last, Compare cmp = Compare{}) {
 	auto const N = std::distance(first, last);
-	if (N <= 1){ 
-        return;
-    }
+	if (N <= 1)  return;
 	auto const pivot = std::next(first, N / 2);
 	std::nth_element(first, pivot, last, cmp);
 	quick_sort(first, pivot, cmp);
