623b91a171fe450fd692bbc4e0e9c81672096ec4
Jack Cho <jack06215@users.noreply.github.com>
2021-09-04 17:36:00 +0800

    fib using STL

---
+++ LeetCode/Tutorial/fib.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <functional>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+auto generate_n_fibonacci(uint64_t n) {
+	vector<uint64_t> fib(n, 1);
+	transform(fib.cbegin(),
+			  fib.cend() - 2,
+			  fib.cbegin() + 1,
+			  fib.begin() + 2,
+			  plus{});
+	return fib;
+}
+
+int main (void) {
+	auto result = generate_n_fibonacci(50);
+	for(const auto& val: result) {
+		cout << val << " ";
+	}
+	cout << endl;
+	return 0;
+}
