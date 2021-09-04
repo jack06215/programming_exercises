a486de4c4d88476838c34c809e9a59498b56e6d6
Jack Cho <jack06215@users.noreply.github.com>
2021-09-04 15:07:49 +0800

    42

---
+++ LeetCode/42. Trapping Rain Water/solution.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <functional>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int main (void) {
+	vector<int> arr = { 1, 2, 3 };
+	auto sum = reduce(arr.cbegin(), arr.cend(), 1, multiplies{});
+	cout << sum << endl;
+	
+	vector<int> v = { 1, 2, 3 };
+	auto rms = transform_reduce(v.cbegin(), v.cend(), v.cbegin(), 0, plus{}, 
+								multiplies{});
+	cout << rms << endl;
+	
+	vector<int> vec = { 2, 1, 2, 4, 2, 3, 5, 2, 4, 7 };
+	vector<int> result(vec.size(), 0);
+	partial_sum(vec.cbegin(), vec.cend(), result.begin(), [](auto a, auto b) { return max(a, b); });
+	auto ans = transform_reduce(vec.cbegin(), vec.cend(), result.cbegin(), 0, plus{}, 
+							[](auto a, auto b) {return abs(a - b);});
+	cout << ans << endl;
+	return 0;
+}
