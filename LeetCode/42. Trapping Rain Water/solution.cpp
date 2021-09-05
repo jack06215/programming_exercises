5f15c78433b74d300012ef5c7b04c77216aceb84
Jack Cho <jack06215@gmail.com>
2021-09-05 23:07:56 +0800

    1046, 42

--- LeetCode/42. Trapping Rain Water/solution.cpp
+++ LeetCode/42. Trapping Rain Water/solution.cpp
@@ -1,25 +1,27 @@
 ﻿#include <iostream>
 #include <functional>
 #include <vector>
+#include <algorithm>
 #include <numeric>
+#include <functional>
 
 using namespace std;
 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+		vector<int> u(height.size(), 0);
+		partial_sum(height.cbegin(), height.cend(), u.begin(), 
+			[](auto a, auto b) { return max(a, b); });
+		int ans = transform_reduce(height.cbegin(), height.cend(), 	u.cbegin(), 0, plus{}, 
+			[](auto a, auto b) { return abs(a - b); });
+		return ans;
+    }
+};
+
 int main (void) {
-	vector<int> arr = { 1, 2, 3 };
-	auto sum = reduce(arr.cbegin(), arr.cend(), 1, multiplies{});
-	cout << sum << endl;
-	
-	vector<int> v = { 1, 2, 3 };
-	auto rms = transform_reduce(v.cbegin(), v.cend(), v.cbegin(), 0, plus{}, 
-								multiplies{});
-	cout << rms << endl;
-	
+	Solution s = Solution();
 	vector<int> vec = { 2, 1, 2, 4, 2, 3, 5, 2, 4, 7 };
-	vector<int> result(vec.size(), 0);
-	partial_sum(vec.cbegin(), vec.cend(), result.begin(), [](auto a, auto b) { return max(a, b); });
-	auto ans = transform_reduce(vec.cbegin(), vec.cend(), result.cbegin(), 0, plus{}, 
-							[](auto a, auto b) {return abs(a - b);});
-	cout << ans << endl;
+	cout << s.trap(vec) << endl;
 	return 0;
 }
--- LeetCode/70. Climbing Stairs/solution.cpp
+++ LeetCode/70. Climbing Stairs/solution.cpp
@@ -1,14 +1,34 @@
 #define CATCH_CONFIG_MAIN
-
 #include "../catch.hpp"
-#include "solution.h"
 #include <iostream>
+#include <vector>
 
+class SolutionDP {
+public:
+    int climbStairs(int n) {
+        std::vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+class SolutionResursive {
+public:
+    int climbStairs(int n) {
+        if (n == 0 | n == 1) {
+            return 1;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
 
 TEST_CASE("70. Climbing Stairs", "[climbStairs]")
 {
-    Solution s;
+    SolutionResursive s;
 
-    // REQUIRE( (s.climbStairs(2) == 2) );
-    // REQUIRE( (s.climbStairs(3) == 3) );
+    REQUIRE( (s.climbStairs(2) == 2) );
+    REQUIRE( (s.climbStairs(3) == 3) );
 }
