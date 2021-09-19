145d4527f94d1d917d83b56d9a7c972998c41d06
Jack Cho <jack06215@gmail.com>
2021-09-19 17:42:32 +0800

    add comment

--- AtCoder/Beginner Contest 174/Log.cpp
+++ AtCoder/Beginner Contest 174/Log.cpp
@@ -4,46 +4,45 @@
 
 using namespace std;
 
-// number of logs
-int n;
+int n; 					// number of logs
+int k;					// number of cuts
+double nums[200010]; 	// lengths of each log
 
-// number of cuts
-int k;
-
-// lengths of each log
-double num[200010];
-
-// small number
+// a very small number
 float eps = numeric_limits<float>::epsilon();
 
-bool can(double len)
-{
-	int64_t rul = 0;
-	for(int i = 0; i < n; i++) {
-		int64_t t = num[i]/len;
-		if(abs(num[i] - t * len) > eps) {
+bool can(double len, int num_cuts) {
+	int64_t res = 0;
+	for(int i = 0; i < n; ++i) {
+		// how many smaller logs can we cut
+		int64_t t = nums[i] / len;
+		if(nums[i] - t * len > eps) {
             t++;
         }
-		rul += t - 1;
-		if(rul > k) return false;
+		// add up to total cuts
+		res += t - 1;
+		// false number of cut is over the given constraint
+		if(res > k) return false;
 	}	
 	return true;
 }
 
-int main()
-{
+int main() {
 	cin >> n >> k;
-	for(int i=0; i<n; i++) {
-        cin >> num[i];
-    }
+	for(int i = 0; i < n; ++i) cin >> nums[i];
 	
 	double left = numeric_limits<double>::min();
     double right = numeric_limits<double>::max();
 	
 	while(left < right - eps) {
-		double mid = (left + right)/2;
-		if(can(mid)) right = mid;
-		else left = mid;
+		double mid = left + (right - left) / 2;
+		// can we cut all logs such that its length is <= `mid` within `k` cuts
+		if(can(mid, k)) {
+			right = mid;
+		}
+		else {
+			left = mid;
+		}
 	}
     cout << ceil(right) << endl;
 	return 0;
