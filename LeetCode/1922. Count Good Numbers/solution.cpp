1fa6dd961cb8e3cf837046d6f79fea9fe1e16fbb
Jack Cho <jack06215@gmail.com>
2021-09-20 19:49:07 +0800

    minor change

--- LeetCode/1922. Count Good Numbers/solution.cpp
+++ LeetCode/1922. Count Good Numbers/solution.cpp
@@ -4,31 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    long long power(long long x, long long y, long long p = 1000000007)
-    {
-        long long res = 1;
-        x = x % p;
-        if (x == 0) 
-            return 0;
- 
-        while (y > 0) {
-            if (y & 1) {
-                res = (res * x) % p;
+    long long modpow(long long base, long long exp, long long modulus){
+        long long result = 1;
+        // long long y = base; 
+        while (exp > 0) {
+            if (exp & 1) { // odd exponent
+                result = (result * base) % modulus; // multiplying with base
             }
-            y >>= 1;
-            x = (x * x) % p;
+            base = (base * base) % modulus; // squaring the base
+            exp >>= 1;
         }
-        return res;
+        return result % modulus;
     }
     
     long long countGoodNumbers(long long n) {
         // 5 possibles for even digit, 4 possibles odd digit
-        long long even = power(5, (n + 1) / 2);
-        long long odd = power(4, n / 2);
+        // => 5^n_even * 4^n_odd
+        long long even = modpow(5, (n + 1) / 2, 1000000007);
+        long long odd = modpow(4, n / 2, 1000000007);
         cout << even << " " << odd << endl;
         long long ans = (even * odd) % 1000000007;
-        return ans;
-        
-        
+        return ans; 
     }
 };
