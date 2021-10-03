610ac57ff674fbb7a9c77006e01a42c7b29d3912
Jack Cho <jack06215@gmail.com>
2021-10-03 15:27:49 +0800

    min rect area

--- LeetCode/939. Minimum Area Rectangle/solution.cpp
+++ LeetCode/939. Minimum Area Rectangle/solution.cpp
@@ -1,45 +1,33 @@
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 #include <limits>
 #include <algorithm>
-#include <iostream>
-#include <string>
-#include "../MurmurHash2.h"
-#include "../prettyprint.h"
 
-namespace std {
-    template <> struct hash<std::pair<int, int>> {
-        inline size_t operator()(const std::pair<int, int> &v) const noexcept {
-            return hash<long long>()(((long long)v.first)^(((long long)v.second) << 32));
-        }
-    };
-}
 using namespace std;
 
 class Solution {
 public:
-    int minAreaRect(vector<vector<int>>& points) {
-        unordered_set<pair<int, int>> seen;
-        const auto inf = numeric_limits<int>::max();
-        int res = inf;
-        for (const auto& p : points) {
-            int x1{p[0]}, y1{p[1]};
-            for (auto[x2, y2] : seen) 
-                if (seen.count({x1, y2}) && seen.count({x2, y1})) {
-                    int area = abs(x1 - x2) * abs(y1 - y2);
-                    res = min(area, res);
-                }
-            seen.emplace(x1, y1);
-        }
-        cout << seen << endl;
-        return res == inf ? 0 : res;
+  int minAreaRect(vector<vector<int>>& points) {    
+    unordered_map<int, unordered_set<int>> s;
+    for (const auto& point : points) {
+        s[point[0]].insert(point[1]);
     }
+    const auto inf = numeric_limits<int>::max();
+    const int n = points.size();
+    int min_area = inf;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int x0{points[i][0]}, y0{points[i][1]};
+            int x1{points[j][0]}, y1{points[j][1]};
+            if (x0 == x1 || y0 == y1) continue;
+
+            int area = abs(x0 - x1) * abs(y0 - y1);
+            if (area > min_area) continue;
+            if (!s[x1].count(y0) || !s[x0].count(y1)) continue;
+            min_area = area;
+        }
+    }
+    return min_area == inf ? 0 : min_area;
+  }
 };
-
-int main(int argc, char const *argv[]) {
-    Solution s;
-    vector<vector<int>> points = {{1,1},{1,3},{3,1},{3,3},{4,1},{4,3}};
-    cout << s.minAreaRect(points) << endl;
-    return 0;
-}
-
