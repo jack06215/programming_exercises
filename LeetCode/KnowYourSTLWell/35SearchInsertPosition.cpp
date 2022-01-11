d8b7c8ab43ed82f78cd24fbca5b6d5a5223d42d7
Jack Cho <jack06215@gmail.com>
2022-01-11 23:03:32 +0800

    review

--- LeetCode/KnowYourSTLWell/35SearchInsertPosition.cpp
+++ LeetCode/KnowYourSTLWell/35SearchInsertPosition.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        vector<int>::iterator insert_iter = lower_bound(nums.begin(), nums.end(), target);
+        auto insert_iter = lower_bound(nums.begin(), nums.end(), target);
         return (distance(nums.begin(), insert_iter));
     }
 };
