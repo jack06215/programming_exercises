9cd845254f85a7f0b1253ef8d826fd7e72054706
Jack Cho <jack06215@gmail.com>
2022-01-15 19:25:40 +0800

    moore voting

---
+++ LeetCode/Observation/169MajorityElement.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // moore's voting algorithm
+        int count = 0;
+        int candidate = nums[0];
+        for (auto& num : nums) {
+            // update new candidate
+            if (count == 0) {
+                candidate = num;
+            }
+            // majority count + 1
+            if (num == candidate) {
+                count++;
+            }
+            // // majority count - 1
+            else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
