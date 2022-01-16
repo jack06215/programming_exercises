4feb761314a1a8f08ff0dd60eac180d2be10c6f7
Jack Cho <jack06215@gmail.com>
2022-01-16 20:15:19 +0800

    review

--- LeetCode/DivideAndConquer/395LongestSubstringwithAtLeastKRepeatingCharacters.cpp
+++ LeetCode/DivideAndConquer/395LongestSubstringwithAtLeastKRepeatingCharacters.cpp
@@ -10,34 +10,33 @@ public:
     }
 public:
     int _longestSubstring(int start, int end, string s, int k) {
-        // store frequency of each character in string
-        vector<int> charCount(26);
+        // frequency of each character in string
+        vector<int> counter(26);
         for (int i = start; i < end; i++) {
-            charCount[s[i] - 'a']++;
+            counter[s[i] - 'a']++;
         }
 
-        // check if frequency of any character in string is less than k
+        // check if substring are all the same, if yes then return the length of substring
         int flag = 1;
         for (int i = 0; i < 26; i++) {
-            if (charCount[i] > 0 && charCount[i] < k) {
+            if (counter[i] > 0 && counter[i] < k) {
                 flag = 0;
             }
         }
-
-        // if frequency of all characters is more than k, return length of string
         if (flag) {
             return end - start;
         }
 
         // split string at character whose frequency is less than k
         for (int i = start; i < end; i++) {
-            int count = charCount[s[i] - 'a'];
+            int count = counter[s[i] - 'a'];
             if (count > 0 && count < k) {
                 int lhs = _longestSubstring(start, i, s, k);
                 int rhs = _longestSubstring(i + 1, end, s, k);
                 return max(lhs, rhs); // return max length from left and right substring
             }
         }
-        return end - start; // return length of string
+        // length of substring
+        return end - start;
     }
 };
