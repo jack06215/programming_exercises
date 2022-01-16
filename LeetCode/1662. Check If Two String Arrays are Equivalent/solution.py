dc0c64903fafa9691dcc3b18eede9f96e33b1913
Jack Cho <jack06215@gmail.com>
2022-01-16 20:37:44 +0800

    clear folders

--- LeetCode/1662. Check If Two String Arrays are Equivalent/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def arrayStringsAreEqual(self, word1: list, word2: list) -> bool:
-        return ''.join(word1) == ''.join(word2)
-
-arr1 = ["ab", "c"]
-arr2 = ["a", "bc"]
-solution = Solution()
-
-print(solution.arrayStringsAreEqual(arr1, arr2))
