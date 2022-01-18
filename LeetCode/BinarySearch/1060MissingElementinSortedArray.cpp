180ed6a6b5ea77bfb160a3b5d49c8aa2b9a41f07
Jack Cho <jack06215@gmail.com>
2022-01-18 22:27:10 +0800

    review

--- LeetCode/BinarySearch/1060MissingElementinSortedArray.cpp
+++ LeetCode/BinarySearch/1060MissingElementinSortedArray.cpp
@@ -9,23 +9,27 @@ public:
         int left = 0;
         int right = nums.size();
 
-        // # of missing numbers in [nums[0], nums[i]]
-        auto n_missing = [&](int i) { return nums[i] - nums[0] - i; };
+        // count of missing numbers in [nums[0], nums[i]]
+        auto count_missing_element = [&](int i) { return nums[i] - nums[0] - i; };
 
-        // find the first index l s.t. nMissing(l) >= k
+        // find the first index such that count_missing_element(left) >= k
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (n_missing(mid) >= k) {
+            // if from [left, mid] the count bigger than k, then k-th element must within LHS
+            if (count_missing_element(mid) >= k) {
                 right = mid;
             }
+            // otherwise we reduce search space
             else {
                 left = mid + 1;
             }
         }
-        return nums[left - 1] + (k - n_missing(left - 1));
+        return nums[left - 1] + (k - count_missing_element(left - 1));
     }
 };
 
+// 7 + (3 - 2) = 8
+// 4 + (1 - 0) = 5
 int main(int argc, char const* argv[]) {
     auto nums = stringToIntVector("[4,7,9,10]");
     cout << Solution().missingElement(nums, 3) << endl;
