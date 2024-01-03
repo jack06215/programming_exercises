f4c471be8fd7700efdceb922c4e5d0bcf651ac13
Jack Cho <jack06215@gmail.com>
2024-01-03 21:01:28 +0900

    revisit and practice

--- LeetCode/Monotonic Stack/402. Remove K Digits.cpp
+++ LeetCode/Monotonic Stack/402. Remove K Digits.cpp
@@ -8,16 +8,19 @@ public:
         string res;
 
         for (auto c : num) {
+            // check if the current digit is smaller than last digit in the `res`
+            // and if there are still `k` characters to be removed
             while (!res.empty() && res.back() > c && k > 0) {
                 res.pop_back();
                 k--;
             }
 
+            // only non-leading zeros are not added to the result
             if (!res.empty() || c != '0') {
                 res.push_back(c);
             }
         }
-
+        // handles the scenario where there are still digits to be removed after the first loop
         while (k > 0 && !res.empty()) {
             k--;
             res.pop_back();
--- LeetCode/Monotonic Stack/84. Largest Rectanglei n Histogram.cpp
+++ LeetCode/Monotonic Stack/84. Largest Rectanglei n Histogram.cpp
@@ -12,13 +12,15 @@ public:
         stack<int> stk;
         int res = 0;
         int i = 0;
-        int size = heights.size();
+        const int size = heights.size();
         while (i < size) {
             if (stk.empty() || heights[i] >= heights[stk.top()]) {
-                stk.push(i++);
+                stk.push(i);
+                i++;
             }
             else {
-                int height = heights[stk.top()]; stk.pop();
+                int height = heights[stk.top()];
+                stk.pop();
                 int width = stk.empty() ? i : i - stk.top() - 1;
                 res = max(res, height * width);
             }
