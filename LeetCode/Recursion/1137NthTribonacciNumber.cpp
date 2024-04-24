6908f40c327b134347dfb3719a30ce9f8491aef2
Jack Cho <jack06215@gmail.com>
2024-04-24 23:41:10 +0900

    code review

--- LeetCode/Recursion/1137NthTribonacciNumber.cpp
+++ LeetCode/Recursion/1137NthTribonacciNumber.cpp
@@ -3,14 +3,6 @@
 using namespace std;
 
 class Solution {
-    vector<int> memo;
-    int _tribonacci(int n) {
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-        memo[n] = _tribonacci(n - 1) + _tribonacci(n - 2) + _tribonacci(n - 3);
-        return memo[n];
-    }
 public:
     int tribonacci(int n) {
         if (n == 0) {
@@ -24,5 +16,14 @@ public:
         memo[1] = 1;
         memo[2] = 1;
         return _tribonacci(n);
+    }
+private:
+    vector<int> memo;
+    int _tribonacci(int n) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = _tribonacci(n - 1) + _tribonacci(n - 2) + _tribonacci(n - 3);
+        return memo[n];
     }
 };
